7b6a408b622dea6adecf659cfee2ae26e0330c1c
Tarun Rathor <tarun_rathor@intuit.com>
2015-08-30 02:44:40 +0530

    Uploads

--- FoodPoliceService/app.js
+++ FoodPoliceService/app.js
@@ -33,7 +33,8 @@ onFileUploadComplete: function (file) {
 app.get('/api/healthcard/uploads/:image', function (req, res){
     
 	var image = req.params.image;
-	var filePath = path.join(__dirname, image);
+	var filePath1 = path.join(__dirname, 'uploads');
+	var filePath = path.join(filePath1, image);
       console.log(filePath);
     var stat = fs.statSync(filePath);
 
